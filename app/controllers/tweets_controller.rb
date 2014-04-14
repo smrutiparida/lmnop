15cb74123dc88e267e67991820ff72ae59670e51
unknown <smruti.parida@gmail.com>
2014-04-14 15:44:11 +0530

    twitter authentication

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,9 @@ class TweetsController < ApplicationController
 
   def auth
     #send a post request
-    twitter_url = "https://api.twitter.com/oauth/request_token/"
+    consumer_secret = "Fhtou0sRRMw5jaGd6TDNAY25q0pvX0kuWhUG12SuZZIg7sVcA9"
+    auth_token_secret ="TWgjxFgp7T0T6GwEv7jAO3FlXLuKKtjPOoaKRYeOWmTSG"
+    twitter_url = "https://api.twitter.com/oauth/request_token"
     post_params = {}
     post_params['oauth_version'] = "1.0"
     post_params['oauth_timestamp'] = Time.now.to_i.to_s
@@ -18,9 +20,16 @@ class TweetsController < ApplicationController
     post_params['oauth_callback'] = URI::encode('http://lmnopapp.com/tweets/my/')
     post_params['oauth_consumer_key'] = "bNEQCVbQ5G5fm1x0TTuWhCYAR"
     post_params['oauth_nonce'] = rand(10 ** 30).to_s.rjust(30,'0')
-    post_params['oauth_signature'] = "POST&" + URI::encode(twitter_url) + "&"
-    data = "OAuth " + post_params.map{|k,v| "#{k}=#{v}"}.join(',')
+    
+    signature_base_string = signature_base_string("POST", twitter_url, post_params)
 
+    logger.info(signature_base_string)
+
+    post_params['oauth_signature'] = url_encode(sign(consumer_secret + '&' + auth_token_secret, signature_base_string))
+
+    data = "OAuth " + post_params.map{|k,v| "#{k}=\"#{v}\""}.join(', ')
+
+    logger.info(data)
     
     uri = URI.parse(twitter_url)
 
@@ -37,6 +46,21 @@ class TweetsController < ApplicationController
 
     #https://api.twitter.com/oauth/authenticate?oauth_token=NPcudxy0yU5T3tBzho7iCotZ3cnetKwcTIRlX0iwRl0
     
+  end
+  
+  def url_encode(string)
+    CGI::escape(string)
+  end
+  
+  def signature_base_string(method, uri, params)
+    encoded_params = params.sort.collect{ |k, v| url_encode("#{k}=#{v}") }.join("%26")
+    method + '&' + url_encode(uri) + '&' + encoded_params
+  end
+  
+  def sign(key, base_string)
+    digest = OpenSSL::Digest::Digest.new('sha1')
+    hmac = OpenSSL::HMAC.digest(digest, key, base_string)
+    Base64.encode64(hmac).chomp.gsub(/\n/, '')
   end
 
 end
