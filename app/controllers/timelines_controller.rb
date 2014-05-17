1c68725a68ecf8398a2b22ef0b5a24f7680020d3
unknown <smruti.parida@gmail.com>
2014-05-18 00:15:23 +0530

    custom timelines

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -7,10 +7,13 @@ require 'twitter'
 
 class TimelinesController < ApplicationController
   def create
-    client = get_auth_client(output_params)
-    response = client.post("https://api.twitter.com/1.1/beta/timelines/custom/create.json", params={:name => "newstest"})
-    Rails.logger.info(response)
-    render :json => response.to_json
+    return redirect_to '/tweets/auth' unless session[:user] or (params["oauth_token"] and params["oauth_verifier"])
+    if session[:user]
+      output_params = session[:user]
+      client = get_auth_client(output_params)
+      response = client.post("https://api.twitter.com/1.1/beta/timelines/custom/create.json", params={:name => "newstest"})
+      Rails.logger.info(response)
+      render :json => response.to_json
   end
 
   def destroy
