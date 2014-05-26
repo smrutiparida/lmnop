c0ba3de8dcbef459e4da6cfcb225c95b5c6cd83e
unknown <smruti.parida@gmail.com>
2014-05-26 13:10:25 +0530

    remove protect forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  #protect_from_forgery
   #the below 2 is from smruti.parida@gmail.com . We got permission to brizztv account for custom timelines account. Hence creating new key and secret
   
 end
