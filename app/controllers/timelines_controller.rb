c0a6eb3d38d5cbf1254697bcade6c16bd1cd98d0
unknown <smruti.parida@gmail.com>
2014-06-19 10:01:22 +0530

    wrote tool to check time of sleep

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,7 +4,7 @@ class TimelinesController < ApplicationController
   def index
     p = Time.now.in_time_zone('Kolkata')
     text = "Not the right time to know this."
-    if p.hour.to_i < 10 or p.hour.to_i > 21 
+    if p.hour.to_i < 11 or p.hour.to_i > 21 
       #request.remote_ip was awake 
       session[:awake] = {} if session[:awake].nil?
       session[:awake].delete(request.remote_ip)
