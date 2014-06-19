8b3551278a092ba5e0427fb4bffc37a6de627002
unknown <smruti.parida@gmail.com>
2014-06-19 09:07:21 +0530

    wrote tool to check time of sleep

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -4,6 +4,8 @@ class TimelinesController < ApplicationController
   def index
     p = Time.now.in_time_zone('Kolkata')
     #request.remote_ip was awake 
+    session[:awake] = {} if session[:awake].nil?
+    Rails.logger.info(request.remote_ip)
     session[:awake][request.remote_ip] =  p.hour.to_s + ":" + p.min.to_s
     render :json => session[:awake].to_json
   end  
